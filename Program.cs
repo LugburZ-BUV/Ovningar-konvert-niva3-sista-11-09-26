e2a39e7587460d7f9b1afd6b8d152e821092f0f1
LugburZ-BUV <Nils-olmas@live.se>
2026-09-11 21:46:18 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Ovningar_konvert_niva3_sista_11_09_26
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
